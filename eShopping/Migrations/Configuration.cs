5d7c2f776fc7f5a35fb55b443bb5a72d75211221
Diogo Guerra <diogoguerra25@gmail.com>
2020-12-14 19:33:45 +0000

    Added: Enabled migrations

---
+++ eShopping/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace eShopping.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eShopping.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(eShopping.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
