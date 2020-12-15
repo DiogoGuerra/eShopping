c4a9152dc85a83502df40f237efcbc1f8af47cb3
Diogo Guerra <diogoguerra25@gmail.com>
2020-12-15 17:40:38 +0000

    Added: Register Name

--- eShopping/Controllers/AccountController.cs
+++ eShopping/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using eShopping.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace eShopping.Controllers
 {
@@ -73,9 +74,16 @@ namespace eShopping.Controllers
                 return View(model);
             }
 
+            string user_name = "";
+            var user = await UserManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                user_name = user.UserName;
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(user_name, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +159,7 @@ namespace eShopping.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Name, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
