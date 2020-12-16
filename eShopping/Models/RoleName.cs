74bcd18e2100deb52a798d2e002522141ea18e08
Diogo Guerra <diogoguerra25@gmail.com>
2020-12-16 17:09:33 +0000

    Added:Role Names

---
+++ eShopping/Models/RoleName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eShopping.Models
+{
+    public class RoleName
+    {
+        public const string Admin = "admin";
+        public const string Company = "company";
+        public const string User = "user";
+        public const string Employee = "employee";
+    }
+}
